c62126416c1441aefd3cebae44f4aefa64e846dc
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-09-30 20:24:09 +0200

    feat: params demo

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 import "github.com/gofiber/fiber/v2"
 func main() {
   app := fiber.New()
-  app.Get("/", func(c *fiber.Ctx) error {
-    return c.SendString("Hello, World :)")
+  app.Get("/user/:name", func(c *fiber.Ctx) error {
+      return c.SendString("Hello "+ c.Params("name"))
   })
   app.Listen(":8080")
 }
