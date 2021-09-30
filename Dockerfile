1fea11309875aa91eabafe45fded249f3166a617
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-09-30 21:02:47 +0200

    feat: demo get/post routes

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM golang:1.17
 
 WORKDIR /go/src/app
+
 COPY . .
 
 RUN go get -u github.com/gofiber/fiber/v2
--- main.go
+++ main.go
@@ -1,9 +1,54 @@
 package main
-import "github.com/gofiber/fiber/v2"
+import (
+   "io/ioutil"
+   "bytes"
+   "log"
+   "net/http"
+   "encoding/json"
+   "github.com/gofiber/fiber/v2"
+)
+
 func main() {
   app := fiber.New()
   app.Get("/user/:name", func(c *fiber.Ctx) error {
+      // c.Set("Connection", "keep-alive")
+      // c.Set("Content-Type", "text/html; charset=utf-8")
       return c.SendString("Hello "+ c.Params("name"))
   })
+
+  app.Get("/dump", func(c *fiber.Ctx) error {
+      resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
+      if err != nil {
+          log.Fatalln(err)
+      }
+      body, err := ioutil.ReadAll(resp.Body)
+      if err != nil {
+          log.Fatalln(err)
+      }
+      return c.SendString(string(body))
+  })
+
+  app.Get("/post", func(c *fiber.Ctx) error {
+      //Encode the data
+      postBody, _ := json.Marshal(map[string]string{
+          "name":  "Toby",
+          "email": "Toby@example.com",
+      })
+      responseBody := bytes.NewBuffer(postBody)
+      //Leverage Go's HTTP Post function to make request
+      resp, err := http.Post("https://postman-echo.com/post", "application/json", responseBody)
+      //Handle Error
+      if err != nil {
+          log.Fatalf("An Error Occured %v", err)
+      }
+      defer resp.Body.Close()
+      //Read the response body
+      body, err := ioutil.ReadAll(resp.Body)
+      if err != nil {
+          log.Fatalln(err)
+      }
+      return c.SendString(string(body))
+  })
+
   app.Listen(":8080")
 }
